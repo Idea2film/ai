2be9d940bfa7c0976b3b2d63c0ac6a9ddbc28024
Idea2film <ideafilm@tutamail.com>
2025-03-13 06:33:54 +0200

    Add files via upload

---
+++ horizontal.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // JavaScript for horizontal orientation
+    function scrollJson(direction) {
+        // Logic to scroll JSON content up or down
+    }
+
+    function changeDesign() {
+        // Logic to switch styles
+    }
+
+    window.scrollJson = scrollJson;
+    window.changeDesign = changeDesign;
+});
---
+++ vertical.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // JavaScript for vertical orientation
+    function prevSection() {
+        // Logic to go to previous section
+    }
+
+    function nextSection() {
+        // Logic to go to next section
+    }
+
+    function changeDesign() {
+        // Logic to switch styles
+    }
+
+    window.prevSection = prevSection;
+    window.nextSection = nextSection;
+    window.changeDesign = changeDesign;
+});
